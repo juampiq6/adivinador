6319aff0d33ef3f081698a05856d886b8a008d71
Juan Pablo <juampiq6@gmail.com>
2019-05-14 16:35:19 -0300

    se implementa metodo para verificar la combinacion, utilizando maps para guardar los numeros ingresados

--- adivinar.go
+++ adivinar.go
@@ -6,17 +6,35 @@ import (
 
 func humanoAdivina() {
 
+	cmbBase := generarCombBase()
 	fmt.Print("\n_______________________________________________________________________________________________\n")
 	fmt.Print("\n                                    Adivine el número:                                     ")
 	fmt.Print("\n_______________________________________________________________________________________________\n")
 	for {
 		num, _ := preguntarNum()
-		bien, reg := 
+		var cmb combinacion
+		bien, reg := verificarCombinacion(cmb, cmbBase)
 		if bien == 4 {
 			fmt.Print("\n *** Felicitaciones, adivino el número! *** ")
 			break
 		} else {
 			fmt.Print("BIEN : ", bien, " - REGULAR : ", reg)
+
 		}
 	}
+}
+
+func verificarCombinacion(cmbIngresada combinacion, cmbBase combinacion) (int, int) {
+	var bien int
+	var reg int
+	for cifra, orden := range cmbIngresada.cifras {
+		if value, exists := cmbBase.cifras[cifra]; exists {
+			if orden == value {
+				bien++
+			} else {
+				reg++
+			}
+		}
+	}
+	return bien, reg
 }
